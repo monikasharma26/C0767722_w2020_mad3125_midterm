f5852370eafbf3be2da463f03f948d966540e4f0
Monika <monikasharma26081991@gmail.com>
2020-04-08 01:43:47 -0400

    [Monika] CRADetails Screen Added

--- app/src/main/java/com/example/C0767722_w2020_mad3125_midterm/Activities/MainActivity.java
+++ app/src/main/java/com/example/C0767722_w2020_mad3125_midterm/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.C0767722_w2020_mad3125_midterm;
+package com.example.C0767722_w2020_mad3125_midterm.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.C0767722_w2020_mad3125_midterm.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/C0767722_w2020_mad3125_midterm/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.C0767722_w2020_mad3125_midterm.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
