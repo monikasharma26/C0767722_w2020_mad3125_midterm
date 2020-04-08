a7e3040adca82ceb52b104eda5e24d8c71ef153d
Monika <monikasharma26081991@gmail.com>
2020-04-08 01:28:48 -0400

    [Monika] InitialCommit

---
+++ app/src/main/java/com/example/C0767722_w2020_mad3125_midterm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.C0767722_w2020_mad3125_midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
