7f678bcf00c7167e56c41b1cf4bed3bc54f28fb2
Monika <monikasharma26081991@gmail.com>
2020-04-09 02:03:26 -0400

    [Monika] CRACustomer Model Class added

--- app/src/main/java/com/example/C0767722_w2020_mad3125_midterm/models/CRACustomer.java
+++ app/src/main/java/com/example/C0767722_w2020_mad3125_midterm/models/CRACustomer.java
@@ -1,6 +1,9 @@
 package com.example.C0767722_w2020_mad3125_midterm.models;
 
 public class CRACustomer {
-   
+    private int sinNumber;
+    private String fName,lName,fullName;
+    private String birthDate;
+
 
 }
