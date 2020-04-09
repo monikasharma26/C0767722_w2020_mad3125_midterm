e625ff879b342b0b17b82fdf09b2a7807ce7804e
Monika <monikasharma26081991@gmail.com>
2020-04-09 02:15:19 -0400

    [Monika] CRACustomer Model Class objects and getter and setter added

---
+++ app/src/main/java/com/example/C0767722_w2020_mad3125_midterm/calculations/Calculator.java
@@ -0,0 +1,4 @@
+package com.example.C0767722_w2020_mad3125_midterm.calculations;
+
+public class Calculator {
+}
--- app/src/main/java/com/example/C0767722_w2020_mad3125_midterm/models/CRACustomer.java
+++ app/src/main/java/com/example/C0767722_w2020_mad3125_midterm/models/CRACustomer.java
@@ -1,9 +1,63 @@
 package com.example.C0767722_w2020_mad3125_midterm.models;
 
-public class CRACustomer {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class CRACustomer  {
     private int sinNumber;
     private String fName,lName,fullName;
-    private String birthDate;
+    private String birthDate,age;
+    private String grossIncome;
+    private String rrsContributed;
+
+
+    public int getSinNumber() {
+        return sinNumber;
+    }
+
+    public void setSinNumber(int sinNumber) {
+        this.sinNumber = sinNumber;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getGrossIncome() {
+        return grossIncome;
+    }
+
+    public void setGrossIncome(String grossIncome) {
+        this.grossIncome = grossIncome;
+    }
+
+    public String getRrsContributed() {
+        return rrsContributed;
+    }
+
+    public void setRrsContributed(String rrsContributed) {
+        this.rrsContributed = rrsContributed;
+    }
 
 
 }
