6158cfd2f4d1a9e415f418c12fb907544b7311e1
Monika <monikasharma26081991@gmail.com>
2020-04-08 20:07:49 -0400

    [Monika] activityMain Design adding

--- app/src/main/java/com/example/C0767722_w2020_mad3125_midterm/Activities/MainActivity.java
+++ app/src/main/java/com/example/C0767722_w2020_mad3125_midterm/Activities/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.C0767722_w2020_mad3125_midterm.Activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
 
 import com.example.C0767722_w2020_mad3125_midterm.R;
 
@@ -11,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       setContentView(R.layout.activity_main);
+
     }
 }
