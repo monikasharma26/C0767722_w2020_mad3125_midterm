c5439879548fbd3b58eca268214d316b2c8170fe
Monika <monikasharma26081991@gmail.com>
2020-04-09 02:17:10 -0400

    [Monika] CRACustomer Implements parceable

--- app/src/main/java/com/example/C0767722_w2020_mad3125_midterm/models/CRACustomer.java
+++ app/src/main/java/com/example/C0767722_w2020_mad3125_midterm/models/CRACustomer.java
@@ -3,13 +3,13 @@ package com.example.C0767722_w2020_mad3125_midterm.models;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public class CRACustomer  {
+public class CRACustomer implements Parcelable {
     private int sinNumber;
     private String fName,lName,fullName;
     private String birthDate,age;
     private String grossIncome;
     private String rrsContributed;
-    
+
     public CRACustomer(int sinNumber, String fName, String lName, String birthDate, String grossIncome, String rrsContributed) {
         this.sinNumber = sinNumber;
         this.fName = fName;
@@ -31,7 +31,17 @@ public class CRACustomer  {
     }
 
 
+    public static final Creator<CRACustomer> CREATOR = new Creator<CRACustomer>() {
+        @Override
+        public CRACustomer createFromParcel(Parcel in) {
+            return new CRACustomer(in);
+        }
 
+        @Override
+        public CRACustomer[] newArray(int size) {
+            return new CRACustomer[size];
+        }
+    };
 
     public int getSinNumber() {
         return sinNumber;
@@ -82,4 +92,20 @@ public class CRACustomer  {
     }
 
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(sinNumber);
+        dest.writeString(fName);
+        dest.writeString(lName);
+        dest.writeString(fullName);
+        dest.writeString(birthDate);
+        dest.writeString(age);
+        dest.writeString(grossIncome);
+        dest.writeString(rrsContributed);
+    }
 }
