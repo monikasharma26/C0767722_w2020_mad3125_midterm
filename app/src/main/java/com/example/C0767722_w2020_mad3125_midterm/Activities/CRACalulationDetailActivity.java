4fbe66156552097a2ed7896ca5c7cc5bc417103d
Monika <monikasharma26081991@gmail.com>
2020-04-09 05:37:30 -0400

    [Monika] Logic added till age in main class

--- app/src/main/java/com/example/C0767722_w2020_mad3125_midterm/Activities/CRACalulationDetailActivity.java
+++ app/src/main/java/com/example/C0767722_w2020_mad3125_midterm/Activities/CRACalulationDetailActivity.java
@@ -1,23 +1,37 @@
 package com.example.C0767722_w2020_mad3125_midterm.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 import com.example.C0767722_w2020_mad3125_midterm.R;
+import com.example.C0767722_w2020_mad3125_midterm.models.CRACustomer;
+import com.example.C0767722_w2020_mad3125_midterm.ui.aboutus.AboutUsFragment;
+import com.example.C0767722_w2020_mad3125_midterm.ui.home.HomeFragment;
+import com.example.C0767722_w2020_mad3125_midterm.ui.taxdetails.TaxDetailsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class CRACalulationDetailActivity extends AppCompatActivity {
-
+         Bundle bundle = new Bundle();
+        private CRACustomer details;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cra_calulation_detail);
+        Intent intent = getIntent();
+         details = intent.getParcelableExtra("detials");
+         bundle.putParcelable("details",details);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+       // getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MapFragment()).commit();
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -29,3 +43,5 @@ public class CRACalulationDetailActivity extends AppCompatActivity {
     }
 
 }
+
+
